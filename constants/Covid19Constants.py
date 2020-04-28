c9165636fb909febb7e5a93121598cf17405bf08
Saurabh Sarup <ssarup@gmail.com>
2020-04-28 18:00:28 -0400

    Downloads will now happen in a different folder.

--- constants/Covid19Constants.py
+++ constants/Covid19Constants.py
@@ -1,6 +1,6 @@
 class Covid19Constants(object):
     US_CONFIRMED_URL          = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
     GLOBAL_CONFIRMED_URL      = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-    DOWNLOAD_FOLDER       = '/Users/developer/Downloads/covid19'
+    DOWNLOAD_FOLDER              = '/Users/developer/Documents/src/python/data/covid19/Downloads'
     US_CONFIRMED_FILE_PREFIX     = 'time_series_covid19_confirmed_US'
     GLOBAL_CONFIRMED_FILE_PREFIX = 'time_series_covid19_confirmed_global'
